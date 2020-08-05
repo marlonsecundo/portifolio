4ae6366461cd08bbbc0e7ad96b5fc70aded8ea17
Marlon Secundo <marlon_secundo@outlook.com>
2020-08-05 10:23:59 -0300

    Updates

---
+++ precache-manifest.d4f5354f040dc08ee7ad40a544a90a4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e36ead885419d11465324130b448328",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65e52500c78d2fac3cf3",
+    "url": "/static/js/2.9b6babb3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9b6babb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7b98e47de9cae698ee9",
+    "url": "/static/js/main.70fd8563.chunk.js"
+  },
+  {
+    "revision": "8f2579ff55bd5aa1997a",
+    "url": "/static/js/runtime-main.87831706.js"
+  }
+]);
---
+++ static/js/main.70fd8563.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpportifolio=this.webpackJsonpportifolio||[]).push([[0],{3:function(e,t,n){e.exports=n(8)},8:function(e,t,n){"use strict";n.r(t);var o=n(0),r=n.n(o),l=n(2);var a=function(){return r.a.createElement("div",null,"Hello World!")};n.n(l).a.render(r.a.createElement(r.a.StrictMode,null,r.a.createElement(a,null)),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.70fd8563.chunk.js.map
